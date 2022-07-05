f82f83ee36449644f8bf017353e1e5e0b4c1a115
SCOUT-METEAM <95029626+SCOUT-METEAM@users.noreply.github.com>
2022-07-05 21:32:39 +0800

    Add files via upload

---
+++ ArduinoCoding/Task02B_RGBLEDBlinking/RGB_Prog03/RGB_Prog03.ino
@@ -0,0 +1,18 @@
+void setup() 
+{
+  for(byte i=2;i<=4;i++)
+  {
+  pinMode(i, OUTPUT);
+  }
+}
+
+void loop() 
+{
+  for (byte i=2;i<=4;i++)
+  {
+   digitalWrite(i, HIGH);
+   delay (1000);
+   digitalWrite(i, LOW);
+   delay (1000);
+  }
+}
