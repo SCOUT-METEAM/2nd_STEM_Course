eab9cf23e3702b3d27cdd40b5bc138c64e3eeccd
SCOUT-METEAM <95029626+SCOUT-METEAM@users.noreply.github.com>
2022-07-23 08:59:33 +0800

    Add files via upload

---
+++ ArduinoCoding/04_Library_Class/04_Library_Class.ino
@@ -0,0 +1,13 @@
+#include "Scout.h" //引入Library檔案
+
+ScoutProg met; //建立物件, 命名物件名稱為met並繼承ScoutProg CLASS。(物件名稱met是自定)
+
+void setup()
+{
+met.meteam(); //引用ScoutProg CLASS的FUNCTION
+}
+
+void loop()
+{
+
+}
